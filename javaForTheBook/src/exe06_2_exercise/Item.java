e2312432fd7e0d818919699c5b9130521cd783c6
Siphosihlemorabe <152267979+Siphosihlemorabe@users.noreply.github.com>
2024-08-29 22:54:03 +0200

    Add files via upload

---
+++ javaForTheBook/src/exe06_2_exercise/Item.java
@@ -0,0 +1,11 @@
+
+package exe06_2_exercise;
+
+public class Item {
+
+        public int itemID;
+        public String desc;
+        public double price;
+        public int quantity;
+        
+}
